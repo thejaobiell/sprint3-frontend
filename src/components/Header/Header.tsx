fee2c01e09e997cbac8912832909c29f7480a28a
João Gabriel <163301770+thejaobiell@users.noreply.github.com>
2024-08-29 18:24:56 -0300

    Add files via upload

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+    <>
+    <header>
+        <a href="iniciar.html">
+        <img id="logo" src="img/portoLogo/Porto Seguro1.png" alt="Logo porto" />
+        </a>
+        <h1 id="Diagnoscar"><span className="black">Diagnos</span> <span className="blue">car</span>
+        </h1>
+    </header>
+    </>
+);
+};
+
+export default Header;
