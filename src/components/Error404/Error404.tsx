a524d48f2b9b2fa7c35c3faa5a5b78915ff49370
LucasLDC <LucasL.Chagas@outlook.com>
2024-09-10 18:27:27 -0300

    Error404

---
+++ src/components/Error404/Error404.tsx
@@ -0,0 +1,14 @@
+import "./Error404.css";
+const erro404 = () => {
+  return (
+    <>
+      <div className="j">
+        <h1 className="a">Erro 404</h1>
+        <h2 className="b">
+          Ops! Infelizmente a página que você tentou acessar não existe.
+        </h2>
+      </div>
+    </>
+  );
+};
+export default erro404;
