ea138ea0b0947dd3974ce4a8786c49410ea567a2
LucasLDC <LucasL.Chagas@outlook.com>
2024-09-14 17:19:32 -0300

    atualização header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,29 @@
 import './Header.css';
+import { Link, useNavigate } from 'react-router-dom';
+
 const Header = () => {
+    
+    const navigate = useNavigate();
+
+    const bmenu = () => {
+    navigate('/menu');
+    };
+    
     return (
-    <>
-    <header>
-        <a href="iniciar.html">
-        <img id="logo" src="img/portoLogo/Porto Seguro1.png" alt="Logo porto" />
-        </a>
-        <h1 id="Diagnoscar"><span className="black">Diagnos</span> <span className="blue">Car</span>
-        </h1>
-    </header>
-    </>
-);
+        <header>
+                <img id="logo" src="img/portoLogo/Porto Seguro1.png" alt="Logo porto" onClick={bmenu}/>
+            <h1 id="Diagnoscar">
+                <span className="black">Diagnos</span> <span className="blue">Car</span>
+            </h1>
+            <nav>
+                <ul>
+                    <li><Link to="/Menu">Menu</Link></li>
+                    <li><Link to="/membros">Membros</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
