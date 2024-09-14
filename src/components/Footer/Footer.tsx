b1863be3f273367d541ce02ea2c556158eca1350
LucasLDC <LucasL.Chagas@outlook.com>
2024-09-14 17:17:21 -0300

    atualização footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
     <>
     <footer>
-        <p>Todos os direitos ainda não reservados © Diagnoscar 2024</p>
+        <p>Todos os direitos ainda não reservados © <a href="https://github.com/thejaobiell/sprint3-frontend/" className='reposito'> Diagnoscar</a> 2024</p>
         <p>João Gabriel Boaventura Marques e Silva | RM:554874</p>
         <p>Lucas de Melo Pinheiro Pinho | RM:558791</p>
         <p>Lucas Leal das Chagas | RM551124</p>
